732447a252b05013a4b5434f1ca9aa95b414af6a
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-06-21 04:08:01 +0500

    Ts changed

--- Arrays/RemoveDuplicatesfromSortedArraySolved/Solution.ts
+++ Arrays/RemoveDuplicatesfromSortedArraySolved/Solution.ts
@@ -1,4 +1,4 @@
-function removeDuplicates(nums: number[]): number {
+function RemoveDuplicates(nums: number[]): number {
     if(nums.length === 0) return 0;
     let x = 1;
     for(let i = 1; i < nums.length; i++) {
